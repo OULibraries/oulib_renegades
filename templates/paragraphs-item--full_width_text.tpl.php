c90c82f3216f4048abce38a07f880f14647ce508
c-corbett <c-corbett@users.noreply.github.com>
2018-06-28 13:23:28 -0500

    Adding class to adjust display

--- templates/paragraphs-item--full_width_text.tpl.php
+++ templates/paragraphs-item--full_width_text.tpl.php
@@ -26,11 +26,13 @@
  * @see template_process()
  */
 ?>
+<div class="container-fluid">
 <div class="paragraph_bundle_full_width row">
 	<div class="col-md-12">
 	<?php print render($content['field_description']); ?>
 	<?php print render($content['field_description1']); ?>
 	</div>
+</div>
 </div>
 
 
